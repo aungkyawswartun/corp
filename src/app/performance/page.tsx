a2bc12f4652ef7e9c5e40f82809cc770ddc297e3
kyawswar <aungkyawswar75@gmail.com>
2024-06-08 15:22:26 +0630

    final

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,13 @@
+import performanceImg from '../../../public/performance.jpg'
+import Hero from '@/components/hero';
+export default function PerformancePage() {
+  return (
+    <div>
+      <Hero
+        imgData={performanceImg}
+        imgAlt="Welding"
+        title="We serve high performance applications."
+      />
+    </div>
+  );
+}
